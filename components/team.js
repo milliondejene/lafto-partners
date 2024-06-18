1669f6a233cd5d74e808f7b3681b91a8171666ce
Million D <milliondejene@protonmail.com>
2024-06-18 15:43:10 +0300

    blog section redesign

--- components/team.js
+++ components/team.js
@@ -12,21 +12,21 @@ function Team() {
         </div>
         <div className="row">
           <div className="col-lg-12">
-            <div className="product-main" style={{marginTop:"80px"}}>
+            <div className="product-main" style={{ marginTop: "80px" }}>
               {[
-                { name: "Jessie Sundberg", role: "Copywriter",img: "/images/p9.jpg"},
+                { name: "Jessie Sundberg", role: "Copywriter", img: "/images/p9.jpg" },
                 { name: "Ephream Sileshi", role: "Account Manager", img: "/images/p2.jpg" },
-                { name: "Ariel Germond", role: "Social Media", img: "/images/p11.jpg"},
-                { name: "Argene", role: "Senior Designer", img: "/images/p8.jpg"},
-                { name: "Janna Sundberg", role: "Chief Editor", img: "/images/p7.jpg"},
-                { name: "Cherinet Kifle", role: "Project Manager", img: "/images/p6.jpg"}
+                { name: "Ariel Germond", role: "Social Media", img: "/images/p11.jpg" },
+                { name: "Argene", role: "Senior Designer", img: "/images/p8.jpg" },
+                { name: "Janna Sundberg", role: "Chief Editor", img: "/images/p7.jpg" },
+                { name: "Cherinet Kifle", role: "Project Manager", img: "/images/p6.jpg" }
               ].map((person, index) => (
                 <div key={index} className="col-lg-3">
                   <div className="product-slider text-center">
                     <div className="pro-img">
-                      <Image src={person.img} alt="product-img" width={190} height={190} />
+                      <Image src={person.img} alt="product-img" layout='responsive' width={190} height={190} />
                     </div>
-                    <div className="border-style" style={{marginTop:"25px"}}>
+                    <div className="border-style" style={{ marginTop: "25px" }}>
                       <h3>{person.name}</h3>
                       <Link href="index2#">{person.role}</Link>
                     </div>
@@ -35,8 +35,12 @@ function Team() {
                         <span>Contact</span>
                       </div>
                       <div className="col-lg-6">
-                      <i style={{marginRight:"10px"}} className="fa fa-envelope" aria-hidden="true"></i>
-                        <i className="fa fa-info-circle" aria-hidden="true"></i>
+                        <Link href="mailto:example@example.com">
+                          <i style={{ marginRight: "10px", color: '#c18b28' }} className="fa fa-envelope clickable-icon" aria-hidden="true"></i>
+                        </Link>
+                        <Link href="/info-page">
+                          <i style={{ color: '#c18b28' }} className="fa fa-info-circle clickable-icon" aria-hidden="true"></i>
+                        </Link>
                       </div>
                     </div>
                   </div>
